e2abf8593fb2820cedd86c636338fddcec96f562
JoshSmith247 <jwsmith23@blakeschool.org>
2021-05-13 14:41:25 -0500

    Update Commands List

--- VCbot.py
+++ VCbot.py
@@ -37,13 +37,16 @@ async def on_message(message):
 
     if message.content.startswith('$help'):
         await message.delete()
-        await message.author.send('Commands:')
+        await message.author.send('Commands (All Users):')
         await message.author.send('$help    - Sends pm of all current commands')
+        await message.author.send('$pack    - Sends pm of resource pack downloads')
+        
+        await message.author.send('Commands (Admin Only):')
         await message.author.send('$enable  - Enables campers to connect to VCs')
         await message.author.send('$disable - Blocks campers from connect to VCs')
 
     if message.content.startswith('$pack'):
-        embed = discord.Embed(title='AiC Server Resource Pack', url='https://github.com/JoshSmith247/RelicBuild-Bukkit-Plugin/releases/download/3.4/AIC_mc_texture_pack_V3.4.zip', description='This is an embed that will show how to build an embed and the different components', color=discord.Color.blue())
+        embed = discord.Embed(title='AiC Server Resource Pack', url='https://github.com/JoshSmith247/RelicBuild-Bukkit-Plugin/releases/download/3.4/AIC_mc_texture_pack_V3.4.zip', description='Adventures In Cardboard Resource Packs', color=discord.Color.blue())
         embed.set_thumbnail(url='https://adventuresincardboard.com/wp-content/uploads/bb-plugin/cache/lastmonsterlivekid-scaled-circle.jpg')
         embed.add_field(name="v. 3.4", value="[Link](https://github.com/JoshSmith247/RelicBuild-Bukkit-Plugin/releases/download/3.4/AIC_mc_texture_pack_V3.4.zip)", inline=True)
         embed.add_field(name="v. 3.3", value="[Link](https://github.com/JoshSmith247/RelicBuild-Bukkit-Plugin/releases/download/3.3/AIC_mc_texture_pack_V3.3.zip)", inline=True)
